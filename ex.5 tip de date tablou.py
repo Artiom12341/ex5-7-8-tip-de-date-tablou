8a77b03d56b4c7ff0f92a2357e427df35f847e9a
Artiom12341 <70652258+Artiom12341@users.noreply.github.com>
2021-09-18 00:34:35 +0300

    Update ex.5 tip de date tablou.py

--- ex.5 tip de date tablou.py
+++ ex.5 tip de date tablou.py
@@ -1,10 +1,15 @@
 
-x=[2,3,4,56,6,7,8]
-y=[2,3,4,5,6,7,8]
-print(x)
-print(y)
-c=x[0]+x[1]+x[2]
-print ("Suma primelor trei componente ale lui x =",c)
+n=int(input("Dati numarul de caratere din primul sir="))
+m=int(input("Dati numarul de caratere din al doilea sir="))
+x=[]
+y=[]
+for i in range(n):
+    b=int(input("x="))
+    x.append(b)
+for i in range(m):
+    c=int(input("y="))
+    y.append(c)
+print ("Suma primelor trei componente ale lui x =",x[0]+x[1]+x[2])
 print("Suma tuturor componentelor a lui y =",sum(y))
 p=1
 for i in range (len(x)):
