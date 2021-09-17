841a8a19c51ec8defbb472aabb248ad2dda0fe93
Artiom12341 <70652258+Artiom12341@users.noreply.github.com>
2021-09-18 00:36:14 +0300

    Update Ex7. tip de date tablou.py

--- Ex7. tip de date tablou.py
+++ Ex7. tip de date tablou.py
@@ -1,7 +1,9 @@
-v=[32,34,33,34,33,34,52]
+v=[]
+for i in range(7):
+    x=int(input("v="))
+    v.append(x)
 z=['luni','Marti','Miercuri','Joi','Vineri','Sambata','Duminica']
-print(v)
 print("venit saptamanal=",sum(v))
 print("Venit zilnic mediu=",sum(v)/7)
-print("Cel mai mare profit zilnic=",z[v.index(max(v))],"=",max(v))
-print("Cel mai mic profit zilnic=",z[v.index(min(v))],"=",min(v))
+print("Cel mai mare profit zilnic,",z[v.index(max(v))],"=",max(v))
+print("Cel mai mic profit zilnic,",z[v.index(min(v))],"=",min(v))
