750cbc0bcb865cc1823de2c8843f8c047e396bd2
Artiom12341 <70652258+Artiom12341@users.noreply.github.com>
2021-09-19 21:20:25 +0300

    Update ex.8 tip de date tablou.py

--- ex.8 tip de date tablou.py
+++ ex.8 tip de date tablou.py
@@ -2,31 +2,26 @@ x=[]
 for i in range(24):
     b=int(input("x="))
     x.append(b)
-z=[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23]
-y=[]
 print("temperatura medie=",sum(x)/24)
 print("temperatura maxima=",max(x),'\n',"temperatura minima=",min(x),)
-y=x.copy()
+z=[]
 if x.count(max(x))>1:
     for i in range(24):
         if x[i]==max(x):
-            print("la ora",z[x.index(max(x))]+1,"Temperatura maxima=",max(x))
-            #del(x[i])
-        else:
-            print("-")
-            #del(x[i])
+            z.append(i+1)
+    for i in z:
+        print("La ora",i,"temperatura maxima=",max(x))    
 else:
-    print("temperatura maxima=",max(x),"la ora",z[x.index(max(x))])
-if y.count(min(y))>1:
+    print("temperatura maxima=",max(x),"la ora",x.index(max(x))+1)
+c=[]
+if x.count(min(x))>1:
     for i in range(24):
-        if y[i]==min(y):
-            print("la ora",z[y.index(min(y))]+1,"Temperatura minima=",min(y))
-            #del(y[i])
-        else:
-            print("-")
-            #del(y[i])
+        if x[i]==min(x):
+            c.append(i+1)
+    for i in c:
+        print("La ora",i,"temperatura minima=",min(x))    
 else:
-    print("temperatura minima=",min(x),"la ora",z[y.index(min(y))])
+    print("temperatura minima=",min(x),"la ora",x.index(min(x))+1)
 
 
 
